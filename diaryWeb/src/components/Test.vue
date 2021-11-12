b9721f88df9a7e093a579c9dc653ac475c8b520e
dongxingyu <dongxingyu@ultrapower.com.cn>
2021-11-12 11:34:32 +0800

    #新增富文本编辑器模板

---
+++ diaryWeb/src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="editor" style="height: 800px;margin-top: 200px"></div>
+</template>
+
+<script>
+import E from "wangeditor"
+
+export default {
+  data() {
+    return {
+      editor: ''
+    }
+  },
+  methods: {
+    setWangEditor() {
+      // 创建编辑器
+      this.editor = new E('#editor')
+      this.editor.create()
+    }
+  },
+  mounted() {
+    this.setWangEditor()
+  }
+}
+</script>
